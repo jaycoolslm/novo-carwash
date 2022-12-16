5c913cfab52d0a806c94d7da6dd2aec3e16f988a
Jake Hall <jake.hallslm@gmail.com>
2022-12-16 07:44:42 +0000

    first push

---
+++ src/lib/components/Stars.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let stars: number = 5;
+	let remainingStars: number = 5 - stars;
+</script>
+
+<div>
+	{#each Array(stars) as _}
+		<i class="fa-solid fa-star" />
+	{/each}
+	{#each Array(remainingStars) as _}
+		<i class="fa-regular fa-star" />
+	{/each}
+</div>
+
+<style>
+	i,
+	i::before,
+	i::after {
+		padding: 0 !important;
+	}
+</style>
